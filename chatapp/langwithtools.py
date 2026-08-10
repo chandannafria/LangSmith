6b5acce809757ced0a02a427b6ad91c3a535f621
Chandan Nafria <chandannafria@gmail.com>
2026-08-10 22:10:46 +0530

    using tools with duckduckgo and positive result

--- chatapp/langwithtools.py
+++ chatapp/langwithtools.py
@@ -1,10 +1,11 @@
 from langchain_core.messages import  BaseMessage , HumanMessage , AIMessage
 from langchain_ollama import OllamaLLM
 from langgraph.graph import StateGraph, START, END
-from langchain_community.tools import Tool, DuckDuckGoSearchRun, WikipediaQueryRun
+from langchain_community.tools import Tool, DuckDuckGoSearchRun, WikipediaQueryRun, wikipedia
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.tools import tool
 
 
 llm = OllamaLLM(
@@ -22,3 +23,34 @@ chain = prompt | llm | StrOutputParser()
 
 # invoke the runnable chain with input
 chain_output = chain.invoke({"topic": "capital of india"})
+
+
+search = DuckDuckGoSearchRun()
+@tool
+def duckduckgo_search(query: str) -> str:
+    """Search the web using DuckDuckGo and return relevant results."""
+    return search.run(query)
+    
+
+# @tool
+# def wikipedia_search(query: str) -> str:
+#     """Search Wikipedia for factual information about a topic."""
+    
+#     try:
+#         return wikipedia.summary(query, sentences=3)
+#     except Exception as e:
+#         return f"Wikipedia search failed: {e}"
+    
+tools = [
+    duckduckgo_search,
+    # wikipedia_search
+]
+
+# llm_with_tool  = llm.bind(tools)
+
+
+print (chain_output)
+
+print(duckduckgo_search.name)
+print(duckduckgo_search.description)
+print(duckduckgo_search.args)
