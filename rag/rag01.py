d888cd65ccfd66a53179bd3ebe4d445dc3ef5bd1
Chandan Nafria <chandannafria@gmail.com>
2026-08-13 13:33:46 +0530

    if a rag file and create a document and textsplit to small chunks and text split to embedding and store the vectorsotre in chroma

---
+++ rag/rag01.py
@@ -0,0 +1,38 @@
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface.embeddings  import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+# FILE UPLOAD 
+file = PyPDFLoader("dl-curriculum.pdf")
+
+docs = file.load()
+# print(docs)
+
+## text splitter
+text_split = RecursiveCharacterTextSplitter(
+    chunk_size = 700,
+    chunk_overlap = 200
+
+)
+chunks = text_split.split_documents(docs)
+
+# print(len(chunks))
+# print(chunks[0])
+
+
+# embedding
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# vectorstore db 
+
+vector_db = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    persist_directory="vectordb"
+)
+print("vector db successfully")
